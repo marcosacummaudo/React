e7dbccbc4682fa7930273667fc36240fcf2d212e
Marcos Cummaudo <marcos.cummaudo@setupinformatica.com.ar>
2023-10-30 17:44:09 -0300

    NavBar

---
+++ ProyectoFinal/src/Componentes/NavBar/NavBar.jsx
@@ -0,0 +1,31 @@
+
+
+export const NavBar = () => {
+  return (
+    <>
+
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid justify-content-center">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+     <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+    
+    </>
+    
+  )
+}
